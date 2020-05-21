5e2ab53e92f21235f02ad56a3036c0fe290486e8
u_mulder <m264695502@gmail.com>
2020-05-21 23:09:28 +0300

    Add code for stage 2

--- app.kt
+++ app.kt
@@ -1,19 +1,58 @@
 package search
 import java.util.*
 
+enum class MESSAGES(val msg: String) {
+    ENTER_PEOPLE_NUM("Enter the number of people:"),
+    ENTER_PEOPLE("Enter all people:"),
+    ENTER_QUERIES_NUM("Enter the number of search queries:"),
+    ENTER_QUERY_DATA("Enter data to search people:"),
+    SEARCH_RESULTS("Found people:"),
+    NO_SEARCH_RESULTS("No matching people found.")
+}
+
+class SearchData {
+    val items = mutableMapOf<Int, String>()
+
+    fun add(item: String) {
+        items[items.size] = item
+    }
+
+    fun search(query: String): Map<Int, String> {
+        return items.filterValues { it.toUpperCase().contains(query.toUpperCase()) }
+    }
+}
+
 fun main() {
     val scanner = Scanner(System.`in`)
-    
-    val items = scanner.nextLine().split(" ")
-    val query = scanner.nextLine()
-    
-    var foundIndex = 0
-    for (index in items.indices) {
-        if (items[index] == query) {
-            foundIndex = index + 1
-            break
-        }
+    val searchData = SearchData()
+
+    var count: Int?
+    do {
+        println(MESSAGES.ENTER_PEOPLE_NUM.msg)
+        count = scanner.nextLine().toIntOrNull()
+    } while (count == null || count < 1)
+
+    println(MESSAGES.ENTER_PEOPLE.msg)
+    repeat(count) {
+        searchData.add(scanner.nextLine())
     }
-    
-    println(if (0 < foundIndex) foundIndex else "Not found")
+    println()
+
+    do {
+        println(MESSAGES.ENTER_QUERIES_NUM.msg)
+        count = scanner.nextLine().toIntOrNull()
+    } while (count == null || count < 1)
+    println()
+
+    repeat(count) {
+        println(MESSAGES.ENTER_QUERY_DATA.msg)
+        val results = searchData.search(scanner.nextLine())
+        if (results.isEmpty()) {
+            println(MESSAGES.NO_SEARCH_RESULTS.msg)
+        } else {
+            println(MESSAGES.SEARCH_RESULTS.msg)
+            results.values.map(::println)
+        }
+        println()
+    }
 }
